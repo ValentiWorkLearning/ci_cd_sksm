1b2b4f35da3aabfe52e9085ab04f76cbb1203def
Valentyn Korniienko <25596072+ValentiWorkLearning@users.noreply.github.com>
2026-05-13 11:56:05 +0300

    Added negative input case

--- factorialTest.cpp
+++ factorialTest.cpp
@@ -12,9 +12,13 @@ EXPECT_EQ(factorial(3), 6);
   EXPECT_EQ(factorial(8), 40320);
 }
 
+TEST(factorialTest, HandleNegativeInput) {
+  EXPECT_EQ(factorial(-10), -1);
+}
+
 
 int main(int argc, char **argv)
 {
-testing::InitGoogleTest(&argc, argv);
+  testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
