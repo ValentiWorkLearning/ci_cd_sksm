78b539775baa963140956f88dcf27cea0b4c0acd
Valentyn Korniienko <25596072+ValentiWorkLearning@users.noreply.github.com>
2026-05-13 11:59:36 +0300

    Throw exception for negative input in factorial
    Add error handling for negative input in factorial function

--- factorial.cpp
+++ factorial.cpp
@@ -1,7 +1,11 @@
 #include "factorial.hpp"
+#include <exception>
 
 int factorial(int n)
 {
+  if (n < 0){
+    throw std::runtime_error("Negative inputs are not supported");
+  }  
   if(n == 0)
       return 1;
 
